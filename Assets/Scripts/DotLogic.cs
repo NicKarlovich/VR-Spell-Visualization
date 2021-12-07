a95059f98bf2e945360136ec8ed34018009dae15
NicKarlovich <nick4pie@gmail.com>
2021-12-07 14:55:06 -0600

    Added Logic to make it so only listens to pointer position while grip is compressed

--- Assets/Scripts/DotLogic.cs
+++ Assets/Scripts/DotLogic.cs
@@ -15,14 +15,20 @@ public class DotLogic : MonoBehaviour
     float maxVelocity = 6.0f;
     Transform grabPoint;
 
+    bool pointing;
+
     public InputActionProperty moveAction;
+    public InputActionProperty grabAction;
 
     // Start is called before the first frame update
     void Start()
     {
+        pointing = false;
         laserPointer.enabled = true;
         selectionDot.GetComponent<MeshRenderer>().enabled = false;
         moveAction.action.performed += Move;
+        grabAction.action.performed += Grab;
+        grabAction.action.canceled += Release;
 
         grabPoint = new GameObject().transform;
         grabPoint.localPosition = new Vector3(0, 0, 0);
@@ -35,40 +41,66 @@ public class DotLogic : MonoBehaviour
     private void OnDestroy()
     {
         moveAction.action.performed -= Move;
+        grabAction.action.performed -= Grab;
+        grabAction.action.canceled -= Release;
     }
 
     // Update is called once per frame
     void Update()
     {
-        selectionDot.GetComponent<MeshRenderer>().enabled = laserPointer.enabled;
-        grabPoint.localPosition = new Vector3(0, 0, grabPoint.localPosition.z);
-        if (laserPointer.enabled)
+        if (pointing)
         {
-            laserPointer.SetPosition(1, new Vector3(0, 0, grabPoint.localPosition.z));
+            selectionDot.GetComponent<MeshRenderer>().enabled = laserPointer.enabled;
+            grabPoint.localPosition = new Vector3(0, 0, grabPoint.localPosition.z);
+            if (laserPointer.enabled)
+            {
+                laserPointer.SetPosition(1, new Vector3(0, 0, grabPoint.localPosition.z));
+            }
+
+            Vector3 grabPos = selectionDot.transform.TransformPoint(selectionDot.transform.localPosition);
+            Debug.Log(grabPos);
+
+            float cubeX = Mathf.Ceil(grabPos.x) - 0.5f;
+            float cubeY = Mathf.Ceil(grabPos.y) - 0.5f;
+            float cubeZ = Mathf.Ceil(grabPos.z) - 0.5f;
+
+            selectionCube.transform.position = new Vector3(cubeX, cubeY, cubeZ);
         }
-
-        Vector3 grabPos = selectionDot.transform.TransformPoint(selectionDot.transform.localPosition);
-        Debug.Log(grabPos);
-
-        float cubeX = Mathf.Ceil(grabPos.x) - 0.5f;
-        float cubeY = Mathf.Ceil(grabPos.y) - 0.5f;
-        float cubeZ = Mathf.Ceil(grabPos.z) - 0.5f;
-
-        selectionCube.transform.position = new Vector3(cubeX, cubeY, cubeZ);
+        
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        Vector2 xMov = Time.deltaTime * maxVelocity * context.action.ReadValue<Vector2>();
+        if (pointing){
+            Vector2 xMov = Time.deltaTime * maxVelocity * context.action.ReadValue<Vector2>();
 
-        grabPoint.transform.Translate(0, 0, -xMov.y);
-        if (grabPoint.localPosition.z < 0.25)
-        {
-            grabPoint.localPosition = new Vector3(0, 0, 0.25f);
+            grabPoint.transform.Translate(0, 0, -xMov.y);
+            if (grabPoint.localPosition.z < 0.25)
+            {
+                grabPoint.localPosition = new Vector3(0, 0, 0.25f);
+            }
         }
+        
 
         //Debug.Log("test?");
         //Debug.Log(context.action.ReadValue<Vector2>());
         //Debug.Log(xMov.y);
+    }
+
+    public void Grab(InputAction.CallbackContext context)
+    {
+        //float gripped = context.action.ReadValue<float>();
+        //Debug.Log(gripped);
+        Debug.Log("grab");
+        pointing = true;
+    }
+
+    public void Release(InputAction.CallbackContext context)
+    {
+        /*float gripped = context.action.ReadValue<float>();
+        Debug.Log(gripped);
+        */
+        Debug.Log("release");
+        pointing = false;
     }
 }
