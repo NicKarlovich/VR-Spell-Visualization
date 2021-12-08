426ab51046c3ebef41ee32f6d730f82a05fd224b
NicKarlovich <nick4pie@gmail.com>
2021-12-07 23:00:17 -0600

    Addex X-Z movement controls to precision movement

--- Assets/Scripts/DotLogic.cs
+++ Assets/Scripts/DotLogic.cs
@@ -8,8 +8,11 @@ public class DotLogic : MonoBehaviour
     public float distanceFromUser = 5.0f;
     public GameObject rightHandController;
     public GameObject selectionDot;
-
     public GameObject selectionCube;
+
+    const float TURN_THRESH = 70f;
+
+    public GameObject headset;
 
     public LineRenderer laserPointer;
     float maxVelocity = 6.0f;
@@ -17,18 +20,41 @@ public class DotLogic : MonoBehaviour
 
     bool pointing;
 
+    bool facingNorth = true;
+    bool facingSouth = false;
+    bool facingEast = false;
+    bool facingWest = false;
+
+    Vector3 sideToSide;
+    Vector3 forwardAndback;
+
+    float xStick;
+    float yStick;
+
     public InputActionProperty moveAction;
     public InputActionProperty grabAction;
+    public InputActionProperty triggerAction;
+
+    bool triggerDown = false;
+    float triggerPull;
+    float runningTimer;
+    int continuousTilesMoved = 0;
+    float timingFactor = .4f;
 
     // Start is called before the first frame update
     void Start()
     {
+        sideToSide = Vector3.zero;
+        forwardAndback = Vector3.zero;
+
         pointing = false;
         laserPointer.enabled = true;
         selectionDot.GetComponent<MeshRenderer>().enabled = false;
         moveAction.action.performed += Move;
         grabAction.action.performed += Grab;
         grabAction.action.canceled += Release;
+        triggerAction.action.performed += PreciseGrab;
+        triggerAction.action.canceled += PreciseRelease;
 
         grabPoint = new GameObject().transform;
         grabPoint.localPosition = new Vector3(0, 0, 0);
@@ -43,11 +69,74 @@ public class DotLogic : MonoBehaviour
         moveAction.action.performed -= Move;
         grabAction.action.performed -= Grab;
         grabAction.action.canceled -= Release;
+        triggerAction.action.performed -= PreciseGrab;
+        triggerAction.action.canceled -= PreciseRelease;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Debug.Log(xStick);
+        Debug.Log(yStick);
+        //Debug.Log(headset.transform.eulerAngles.y);
+        //rotation angle
+        float rotA = headset.transform.eulerAngles.y;
+        /* 
+         * North is +
+         * East is  +x
+         * South is -x
+         * West is  -z
+         */
+        
+        if(facingNorth)
+        {
+            facingNorth = (rotA >= (360 - TURN_THRESH) || rotA <= (0 + TURN_THRESH));
+        } else if (facingEast)
+        {
+            facingEast = (rotA >= (90 - TURN_THRESH) && rotA <= (90 + TURN_THRESH));
+        } else if (facingSouth)
+        {
+            facingSouth = (rotA >= (180 - TURN_THRESH) && rotA <= (180 + TURN_THRESH));
+        } else if (facingWest)
+        {
+            facingWest = (rotA >= (270 - TURN_THRESH) && rotA <= (270 + TURN_THRESH));
+        } else
+        {
+            //Debug.Log("Oof, somehow got here, shouldn't be here");
+        }
+
+
+        if (!(facingNorth || facingEast || facingSouth || facingWest))
+        {
+            // went outside boundries of direction we were facing, now determine where the user should be facing
+            //Debug.Log((rotA <= 225 && rotA >= 135));
+            if(rotA <= 45 || rotA >= 315)
+            {
+                facingNorth = true;
+            }
+            if (rotA <= 135 && rotA >= 45)
+            {
+                facingEast = true;
+            }
+            if (rotA <= 225 && rotA >= 135)
+            {
+                facingSouth = true;
+            }
+            if (rotA <= 315 && rotA >= 225)
+            {
+                facingWest = true;
+            }
+        }
+
+        /*
+        Debug.Log("North: " + facingNorth);
+        Debug.Log("East: " + facingEast);
+        Debug.Log("South: " + facingSouth);
+        Debug.Log("West: " + facingWest);
+        //Debug.Log("potato: " + facingWest);*/
+
+
         if (pointing)
         {
             selectionDot.GetComponent<MeshRenderer>().enabled = laserPointer.enabled;
@@ -58,27 +147,59 @@ public class DotLogic : MonoBehaviour
             }
 
             Vector3 grabPos = selectionDot.transform.TransformPoint(selectionDot.transform.localPosition);
-            Debug.Log(grabPos);
+            //Debug.Log(grabPos);
 
             float cubeX = Mathf.Ceil(grabPos.x) - 0.5f;
             float cubeY = Mathf.Ceil(grabPos.y) - 0.5f;
             float cubeZ = Mathf.Ceil(grabPos.z) - 0.5f;
 
             selectionCube.transform.position = new Vector3(cubeX, cubeY, cubeZ);
+        } else
+        {
+            if(triggerDown)
+            {
+                runningTimer = Time.time;
+                if(runningTimer - triggerPull > timingFactor)
+                {
+                    triggerPull = runningTimer;
+                    doMovement();
+                    continuousTilesMoved++;
+                    if(continuousTilesMoved == 2)
+                    {
+                        timingFactor = 0.2f;
+                    }
+                    if (continuousTilesMoved == 3)
+                    {
+                        timingFactor = 0.17f;
+                    }
+                    if (continuousTilesMoved == 4)
+                    {
+                        timingFactor = 0.13f;
+                    }
+                    if (continuousTilesMoved == 7)
+                    {
+                        timingFactor = 0.1f;
+                    }
+                }
+            }
         }
-        
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         if (pointing){
-            Vector2 xMov = Time.deltaTime * maxVelocity * context.action.ReadValue<Vector2>();
+            Vector2 Mov = Time.deltaTime * maxVelocity * context.action.ReadValue<Vector2>();
 
-            grabPoint.transform.Translate(0, 0, -xMov.y);
+            grabPoint.transform.Translate(0, 0, -Mov.y);
             if (grabPoint.localPosition.z < 0.25)
             {
                 grabPoint.localPosition = new Vector3(0, 0, 0.25f);
             }
+        } else
+        {
+            Vector2 thumbstick = context.action.ReadValue<Vector2>();
+            xStick = thumbstick.x;
+            yStick = thumbstick.y;
         }
         
 
@@ -86,6 +207,72 @@ public class DotLogic : MonoBehaviour
         //Debug.Log(context.action.ReadValue<Vector2>());
         //Debug.Log(xMov.y);
     }
+    public void doMovement()
+    {
+        if (facingNorth)
+        {
+            sideToSide = new Vector3(1, 0, 0);
+            forwardAndback = new Vector3(0, 0, 1);
+        }
+        if (facingSouth)
+        {
+            sideToSide = new Vector3(-1, 0, 0);
+            forwardAndback = new Vector3(0, 0, -1);
+        }
+        if (facingEast)
+        {
+            sideToSide = new Vector3(0, 0, -1);
+            forwardAndback = new Vector3(1, 0, 0);
+        }
+        if (facingWest)
+        {
+            sideToSide = new Vector3(0, 0, 1);
+            forwardAndback = new Vector3(-1, 0, 0);
+        }
+
+        Debug.Log(xStick);
+        Debug.Log(yStick);
+        Vector3 currentCube = selectionCube.transform.position;
+        if (xStick > 0.8)
+        {
+            selectionCube.transform.position = currentCube + sideToSide;
+        }
+        if (xStick < -0.8)
+        {
+            selectionCube.transform.position = currentCube - sideToSide;
+        }
+        if (yStick > 0.8)
+        {
+            selectionCube.transform.position = currentCube + forwardAndback;
+        }
+        if (yStick < -0.8)
+        {
+            selectionCube.transform.position = currentCube - forwardAndback;
+        }
+    }
+
+    public void PreciseGrab(InputAction.CallbackContext context)
+    {
+        continuousTilesMoved = 0;
+        timingFactor = .4f;
+        triggerDown = true;
+        triggerPull = Time.time;
+        runningTimer = Time.time;
+        Debug.Log("North: " + facingNorth);
+        Debug.Log("East: " + facingEast);
+        Debug.Log("South: " + facingSouth);
+        Debug.Log("West: " + facingWest);
+        doMovement();
+        Debug.Log("precise Grab");
+    }
+
+    public void PreciseRelease(InputAction.CallbackContext context)
+    {
+        //don't really care when this happens, it'll happen before preciseGrab again but we don't care when.
+        Debug.Log("precise Release");
+        triggerDown = false;
+    }
+
 
     public void Grab(InputAction.CallbackContext context)
     {
